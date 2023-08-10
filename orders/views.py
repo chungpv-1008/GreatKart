5708870eaf50d489ea9b334e1ceb7ae0bdca4707
tridn <tridnguyen97@gmail.com>
2023-08-10 10:17:04 +0700

    add momo payment

--- orders/views.py
+++ orders/views.py
@@ -173,3 +173,123 @@ def order_complete(request):
         return render(request, 'orders/order_complete.html', context)
     except Exception:
         return redirect('home')
+
+def momo_payment(request):
+    import json
+    import urllib.request
+    import urllib
+    import uuid
+    import requests
+    import hmac
+    import hashlib
+
+    current_user = request.user
+    # If the cart count is less than or equal to 0, then redirect back to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total) / 100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # Store all the billing information inside Order table
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # Generate order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")     # 20210305
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+
+    # parameters send to MoMo get get payUrl
+    endpoint = "https://test-payment.momo.vn/v2/gateway/api/create"
+    partnerCode = "MOMO59AK20220907"
+    accessKey = "zhk7tDktfUZmeR4Z"
+    secretKey = "jKVBar49casmn7qyV5XlLVytM7gGpSDh"
+    orderInfo = "pay with MoMo"
+    redirectUrl = "https://webhook.site/994889cf-8cb2-4f7a-8748-ed2a1551793c"
+    ipnUrl = "https://webhook.site/994889cf-8cb2-4f7a-8748-ed2a1551793c"
+    amount = "50000"
+    orderId = str(uuid.uuid4())
+    requestId = str(uuid.uuid4())
+    requestType = "captureWallet"
+    extraData = ""  # pass empty value or Encode base64 JsonString
+
+    # before sign HMAC SHA256 with format: accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl
+    # &orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId
+    # &requestType=$requestType
+    rawSignature = (f"accessKey={accessKey}" +
+                    f"&amount={amount}" +
+                    f"&extraData={extraData}" + 
+                    f"&ipnUrl={ipnUrl}" +
+                    f"&orderId={orderId}" +
+                    f"&orderInfo={orderInfo}" +
+                    f"&partnerCode={partnerCode}" +
+                    f"&redirectUrl={redirectUrl}" +
+                    f"&requestId={requestId}" +
+                    f"&requestType={requestType}")  
+
+    # puts raw signature
+    # signature
+    h = hmac.new(bytes(secretKey, 'ascii'), bytes(rawSignature, 'ascii'), hashlib.sha256)
+    signature = h.hexdigest()
+    # json object send to MoMo endpoint
+
+    data = {
+        'partnerCode': partnerCode,
+        'partnerName': "Test",
+        'storeId': "MomoTestStore",
+        'requestId': requestId,
+        'amount': amount,
+        'orderId': orderId,
+        'orderInfo': orderInfo,
+        'redirectUrl': redirectUrl,
+        'ipnUrl': ipnUrl,
+        'lang': "vi",
+        'extraData': extraData,
+        'requestType': requestType,
+        'signature': signature
+    }
+    print("--------------------JSON REQUEST----------------\n")
+    data = json.dumps(data)
+    print(data)
+
+    clen = len(data)
+    response = requests.post(endpoint, data=data, headers={'Content-Type': 'application/json', 'Content-Length': str(clen)})
+    redirect(response.json()['payUrl'])
